e66b17776a20dae5476fd483bb0277d8e6c12eb6
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-10-10 11:43:01 +0100

    Send subset of video's frames to (rate limited) API and write csv

--- msface.py
+++ msface.py
@@ -4,52 +4,87 @@ import httplib, urllib, base64, json
 import pickle
 import pandas as pd
 import argparse
+import cv2
+import sys
+from limit import limit
+import os
 
+@limit(20,60)
+def getFaceInfo(infile):
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': api_key["subscription_key"],
+    }
+
+    # Request parameters.
+    params = urllib.urlencode({
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'true',
+        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,makeup,occlusion,blur,exposure,noise',
+    })
+
+    # The URL of a JPEG image to analyze.
+    #body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
+    try:
+        body = open(infile, "rb").read()
+        # Execute the REST API call and get the response.
+        conn = httplib.HTTPSConnection(api_key["uri_base"])
+        conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed = json.loads(data)
+        conn.close()
+
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+    return parsed
 
 parser = argparse.ArgumentParser(description = "Send video frames to MS Face API and return API results to a csv")
 parser.add_argument("--videofile", help = "The input video to send to the MS Face API", required = True)
-parser.add_argument("--startframe",help = "The first frame to send to the API", required = True)
-parser.add_argument("--endframe", help = "The final frame to send to the API", required = True)
+parser.add_argument("--outfile", required = False,
+        help = "Optional output filename.  Will be --videofile with .msface by default (ignoring the path of --videofile)")
+parser.add_argument("--startframe",
+        help = "The first frame to send to the API", required = True,
+        type  = int)
+parser.add_argument("--endframe",
+         help = "The final frame to send to the API", required = True,
+         type = int)
 parser.add_argument("--APIpickle", help = "A pickle containing the API key and region", default = "api.p")
 
 args = parser.parse_args()
+if args.endframe < args.startframe:
+    print("Invalid frame range")
+    sys.exit()
+
+if args.outfile is None:
+   args.outfile = os.path.splitext(os.path.basename(args.videofile))[0] + ".msface" 
 
 # You should generate api.p using writeapi.py
 api_key = pickle.load(open(args.APIpickle, "rb"))
 
-headers = {
-    'Content-Type': 'application/octet-stream',
-    'Ocp-Apim-Subscription-Key': api_key["subscription_key"],
-}
+video = cv2.VideoCapture(args.videofile)
+trackingdata = pd.DataFrame()
 
-# Request parameters.
-params = urllib.urlencode({
-    'returnFaceId': 'false',
-    'returnFaceLandmarks': 'true',
-    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
-})
+for frame in range(args.startframe, args.endframe + 1):
+    video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame)
+    ret, img = video.read()
+    if ret == False:
+        print("Failed to capture frame %s" % frame)
+    cv2.imwrite("tmp.png", img)
+    print("Captured frame %s" % frame)
 
-# The URL of a JPEG image to analyze.
-#body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
-body = open("frames/output_0150.png", "rb").read()
-try:
-    # Execute the REST API call and get the response.
-    conn = httplib.HTTPSConnection(api_key["uri_base"])
-    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
+    apiresp = getFaceInfo("tmp.png")
+    # print (json.dumps(apiresp, sort_keys=True, indent=2))
+    df = pd.io.json.json_normalize(apiresp)
 
-    # 'data' contains the JSON data. The following formats the JSON data for display.
-    parsed = json.loads(data)
-    print ("Response:")
-    print (json.dumps(parsed, sort_keys=True, indent=2))
-    conn.close()
+    df["frame"] = frame
+    df.set_index("frame", inplace = True)
 
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    trackingdata = trackingdata.append(df, ignore_index = False)
+    
+trackingdata.to_csv(args.outfile)
 
-df = pd.io.json.json_normalize(parsed)
-print df
-
-####################################
 
